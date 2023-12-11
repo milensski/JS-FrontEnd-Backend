cbe2a084c501f8008338d1e2bd78f9a619c5756f
Milen Palachorov <feniks9511@gmail.com>
2023-12-11 18:13:27 +0200

    adding exercises

--- DOM-IntroductionExercises/06. Format the Text/solution.js
+++ DOM-IntroductionExercises/06. Format the Text/solution.js
@@ -1,3 +1,18 @@
 function solve() {
-  //TODO
+  let textArea = document.getElementById("input").value.split(".");
+  let outputElement = document.getElementById("output");
+
+  outputElement.textContent = "";
+
+  textArea = textArea
+  .filter((e) => e.length > 1)
+  .map(е => е += ".");
+
+  while (textArea.length > 0) {
+    let p = document.createElement("p");
+
+    p.textContent = textArea.splice(0, 3).join("");
+
+    outputElement.appendChild(p);
+  }
 }
--- DOM-IntroductionExercises/07. Hell's Kitchen/app.js
+++ DOM-IntroductionExercises/07. Hell's Kitchen/app.js
@@ -1,8 +1,67 @@
 function solve() {
-   document.querySelector('#btnSend').addEventListener('click', onClick);
+  document.querySelector("#btnSend").addEventListener("click", onClick);
 
-   function onClick () {
-      //   TODO:
-      
-   }
+  function onClick() {
+    let textArea = document.querySelector("#inputs textarea").value;
+
+    textArea = JSON.parse(textArea);
+
+    let restaurants = {};
+
+    for (const element of textArea) {
+      let [restaurantName, workersInfo] = element.split(" - ");
+      if (!restaurants.hasOwnProperty(restaurantName)) {
+        restaurants[restaurantName] = {};
+      }
+      workersDetails = workersInfo.split(", ");
+      for (const worker of workersDetails) {
+        let [name, salary] = worker.split(" ");
+        salary = Number(salary);
+
+        restaurants[restaurantName][name] = salary;
+      }
+    }
+
+    let bestSalary = 0;
+    let avgSalary = 0;
+    let bestRestaurant = "";
+
+    for (const restaurant in restaurants) {
+      let currentBestSalary = 0;
+      let currentAvgSalary = 0;
+      let totalSalary = 0;
+      for (const salary of Object.values(restaurants[restaurant])) {
+        totalSalary += salary;
+        if (salary > currentBestSalary) {
+          currentBestSalary = salary;
+        }
+      }
+
+      currentAvgSalary =
+        totalSalary / Object.keys(restaurants[restaurant]).length;
+
+      if (currentAvgSalary > avgSalary) {
+        avgSalary = currentAvgSalary;
+        bestSalary = currentBestSalary;
+        bestRestaurant = restaurant;
+      }
+    }
+
+    const sortedSalaries = Object.entries(restaurants[bestRestaurant]).sort((a,b) => b[1] - a[1]);
+
+    let outputBestRestaurant = document.querySelector("#bestRestaurant p");
+    let outputWorkers = document.querySelector("#workers p");
+
+    outputBestRestaurant.textContent = `Name: ${bestRestaurant} Average Salary: ${avgSalary.toFixed(
+      2
+    )} Best Salary: ${bestSalary.toFixed(2)}`;
+    
+    let result = ''
+
+    for (const worker of sortedSalaries) {
+      result += `Name: ${worker[0]} With Salary: ${worker[1]} `;
+    }
+
+    outputWorkers.textContent = result
+  }
 }
