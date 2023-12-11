1ee146c2be4f61591b478b782a828a888ac91c80
Milen Palachorov <feniks9511@gmail.com>
2023-12-11 10:43:32 +0200

    adding exercises

--- DOM-IntroductionExercises/03. Accordion/accordion.js
+++ DOM-IntroductionExercises/03. Accordion/accordion.js
@@ -1,3 +1,13 @@
 function toggle() {
-    console.log('TODO:...');
+  button = document.getElementsByClassName("button")[0];
+  expandedContent = document.getElementById("extra");
+
+  if (button.textContent === "More") {
+    expandedContent.style.display = "block";
+    button.textContent = "Less";
+
+  } else {
+    button.textContent = "More"
+    expandedContent.style.display = "none";
+  }
 }
--- DOM-IntroductionExercises/04. Search in List/search.js
+++ DOM-IntroductionExercises/04. Search in List/search.js
@@ -1,3 +1,22 @@
 function search() {
-   // TODO
+   
+   townsList = document.getElementById("towns").getElementsByTagName("li")
+   searchText = document.getElementById("searchText").value;
+   result = document.getElementById("result")
+   let countMatches = 0
+
+   for (const element of Array.from(townsList)) {
+      element.style.fontWeight = "";
+      element.style.textDecoration = "";
+
+      
+      if (element.textContent.includes(searchText)) {
+         element.style.fontWeight = "bold";
+         element.style.textDecoration = "underline";
+         countMatches++
+      }
+   }
+
+   result.textContent = `${countMatches} matches found`
+
 }
