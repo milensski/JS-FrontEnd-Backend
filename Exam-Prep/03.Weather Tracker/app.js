66ab83d26f4ddfa9f2984701aea83e2b5bad448a
Milen Palachorov <feniks9511@gmail.com>
2023-12-15 10:17:13 +0200

    adding exercises for exam prep

--- Exam-Prep/03.Weather Tracker/app.js
+++ Exam-Prep/03.Weather Tracker/app.js
@@ -9,31 +9,36 @@ const temperatureField = document.getElementById("temperature");
 const dateField = document.getElementById("date");
 
 const historyContainer = document.getElementById("list");
+historyContainer.innerHTML = "";
 
 loadHistoryButton.addEventListener("click", getData);
 addButton.addEventListener("click", postData);
+editButton.addEventListener("click", editData)
+
 function getData() {
   fetch(baseURL)
     .then((r) => r.json())
     .then((response) => {
-      console.log(response);
-      historyContainer.innerHTML = "";
+      
+      historyContainer.innerHTML = ""; 
       for (const townObj of Object.values(response)) {
         const container = document.createElement("div");
         container.className = "container";
+        container.setAttribute("data-townid", `${townObj._id}`)
         container.innerHTML = `
-        <div class="container">
             <h2>${townObj.location}</h2>
             <h3>${townObj.date}</h3>
             <h3 id="celsius">${townObj.temperature}</h3>
-        </div>`;
+        `;
 
         const changeButton = document.createElement('button')
         changeButton.className = "change-btn";
         changeButton.textContent = 'Change'
+        changeButton.addEventListener('click', editWeather)
         const deleteButton = document.createElement("button");
         deleteButton.className = "delete-btn";
         deleteButton.textContent = "Delete";
+        deleteButton.addEventListener('click', deleteData)
 
 
         const divForButtons = document.createElement("div");
@@ -64,8 +69,59 @@ function postData() {
   });
 }
 
+function editData() {
+
+    const townID = locationField.dataset.townid;
+
+    fetch(baseURL + townID, {
+      method: "PUT",
+      body: JSON.stringify({
+        _id: townID,
+        location: locationField.value,
+        temperature: temperatureField.value,
+        date: dateField.value,
+      }),
+    })
+      .then(getData)
+      .then(
+        locationField.removeAttribute("data-townid"),
+        editButton.setAttribute("disabled", "disabled"),
+        addButton.removeAttribute("disabled")
+      )
+      .then(clearInputFields);
+}
+
+function deleteData(e) {
+    const deleteButton = e.currentTarget;
+    const record = deleteButton.parentNode.parentNode;
+    const townID = record.dataset.townid;
+
+    fetch(baseURL + townID, {
+      method: "DELETE",
+    })
+      .then(getData);
+}
+
+function editWeather(e){
+
+    const changeButton = e.currentTarget
+    const record = changeButton.parentNode.parentNode;
+    const townID = record.dataset.townid
+    locationField.value = record.children[0].outerText;
+    temperatureField.value = record.children[2].outerText;
+    dateField.value = record.children[1].outerText;
+
+    locationField.setAttribute("data-townid", townID);
+
+    addButton.setAttribute('disabled', 'disabled')
+    editButton.removeAttribute("disabled");
+    record.remove();
+
+}
+
 function clearInputFields() {
   locationField.value = "";
   temperatureField.value = "";
   dateField.value = "";
 }
+
