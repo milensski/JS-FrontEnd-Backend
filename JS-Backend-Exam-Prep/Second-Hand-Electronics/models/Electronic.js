b6ee8ee7cf5cfc4a99d95f0c86e7caace3251dcd
Milen Palachorov <feniks9511@gmail.com>
2024-02-12 17:21:15 +0200

    creating the electronics model

---
+++ JS-Backend-Exam-Prep/Second-Hand-Electronics/models/Electronic.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+
+const electronicSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  damages: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  production: {
+    type: Number,
+    required: true,
+  },
+  exploitation: {
+    type: Number,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  buyingList: [
+    {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  owner: {
+    type: mongoose.Types.ObjectId,
+    ref: "User",
+  },
+});
